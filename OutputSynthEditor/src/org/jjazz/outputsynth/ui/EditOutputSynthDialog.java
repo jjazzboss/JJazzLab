b8469cf43e8a8aea8bcd6e99b1f6cde9d16c170d
Jerome Lelasseux <contact@jjazzlab.com>
2020-02-24 22:28:04 +0100

    Minor UI fixes.

--- OutputSynthEditor/src/org/jjazz/outputsynth/ui/EditOutputSynthDialog.java
+++ OutputSynthEditor/src/org/jjazz/outputsynth/ui/EditOutputSynthDialog.java
@@ -303,12 +303,12 @@ public class EditOutputSynthDialog extends javax.swing.JDialog implements Proper
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(editor, javax.swing.GroupLayout.DEFAULT_SIZE, 804, Short.MAX_VALUE)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(tf_configName, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btn_Save, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(btn_Save)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
