5167b4826b264711a2f07d1d8bb5f4ca948a92fb
Jerome Lelasseux <jl@jjazzlab.com>
2021-07-27 20:26:33 +0200

    Fake change

--- ColorSetManager/src/org/jjazz/ui/colorsetmanager/ColorSetManagerImpl.java
+++ ColorSetManager/src/org/jjazz/ui/colorsetmanager/ColorSetManagerImpl.java
@@ -55,7 +55,7 @@ public class ColorSetManagerImpl implements ColorSetManager
     // private final Color WHITE = new Color(251, 248, 245);        // "our" white = "old paper"
     // private final Color WHITE = new Color(235, 232, 225);        // "our" white for FlatLAF
     // 
-    // Application variables
+    // Application variables -
     private static final int NB_COLORS = 6;
     private static final String COLOR_PROP_PREFIX = "REFCOLOR-";
     private static ColorSetManagerImpl INSTANCE;
