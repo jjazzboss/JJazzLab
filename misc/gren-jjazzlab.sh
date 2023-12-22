f69433b4ab5989376cb26aa5d64377a197b274bf
Jerome Lelasseux <jl@jjazzlab.com>
2023-12-22 23:45:49 +0100

    Improve gren

--- misc/gren-jjazzlab.sh
+++ misc/gren-jjazzlab.sh
@@ -1,4 +1,15 @@
-gren release --override -c misc/gren.cfg --tags=4.0-BETA2..3.2.1
+if [ ! -d misc ]
+then
+    echo "Invalid start directory. Must be run at jjazzlab project root dir."
+    exit
+fi
+if [[ $# = 0 ]]
+then 
+    echo "EXAMPLE USAGE: misc/gren-jjazzlab.sh 4.0.2"
+    exit
+fi
+gren release --override -c misc/gren.cfg --only-milestones --milestone-match="$1"
+
 
 
 
