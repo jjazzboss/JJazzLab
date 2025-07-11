42a084ae9930969e9a0681ee8b87ab264c2dcb41
Jerome Lelasseux <jl@jjazzlab.org>
2025-07-11 22:21:59 +0200

    NON COMPILABLE - start CompositeRhythm edit

---
+++ plugins/YamJJazz/src/main/java/org/jjazz/yamjjazz/rhythm/api/YjzCompositeRhythm.java
@@ -0,0 +1,46 @@
+/*
+ * 
+ *   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *  
+ *   Copyright @2019 Jerome Lelasseux. All rights reserved.
+ * 
+ *   This file is part of the JJazzLab software.
+ *    
+ *   JJazzLab is free software: you can redistribute it and/or modify
+ *   it under the terms of the Lesser GNU General Public License (LGPLv3) 
+ *   as published by the Free Software Foundation, either version 3 of the License, 
+ *   or (at your option) any later version.
+ * 
+ *   JJazzLab is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *  
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with JJazzLab.  If not, see <https://www.gnu.org/licenses/>
+ *  
+ *   Contributor(s): 
+ * 
+ */
+package org.jjazz.yamjjazz.rhythm.api;
+
+import com.google.common.collect.Multimap;
+import org.jjazz.rhythm.api.RhythmVoice;
+import org.jjazz.rhythmmusicgeneration.spi.MusicGenerator;
+
+/**
+ * A YamJJazzRhythm extended to use different MusicGenerators for specific RhythmVoices.
+ */
+public interface YjzCompositeRhythm extends YamJJazzRhythm
+{
+
+    YamJJazzRhythm getBaseYamJJazzRhythm();
+
+    /**
+     * Get the MusicGenerators to be used, and the associated RhythmVoice list.
+     *
+     * @return
+     */
+    Multimap<MusicGenerator, RhythmVoice> getGenerators();
+    
+}
